1c37df986bafcc8aa30f104b67c5b8b552413b61
Ana Cunha <1771610+anacunha@users.noreply.github.com>
2022-01-23 02:27:46 +0000

    Store entries on DynamoDB and sends SMS

--- src/main/java/com/example/handlingformsubmission/FormController.java
+++ src/main/java/com/example/handlingformsubmission/FormController.java
@@ -9,6 +9,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class FormController {
 
+    private final DynamoDbEnhanced dynamoDb;
+    private final PublishTextSMS publishText;
+
+    public FormController(final DynamoDbEnhanced dynamoDb, final PublishTextSMS publishText) {
+        this.dynamoDb = dynamoDb;
+        this.publishText = publishText;
+    }
+
     @GetMapping("/")
     public String contactForm(final Model model) {
         model.addAttribute("form", new Form());
@@ -17,7 +25,8 @@ public class FormController {
 
     @PostMapping("/form")
     public String formSubmit(@ModelAttribute final Form form) {
-
+        dynamoDb.injectDynamoItem(form);
+        publishText.sendMessage(form.getId());
         return "result";
     }
 }
